285d98175a6746e2ddcde6f1e6d6f31885446495
mohammad keshavarz <m.k.dev@outlook.com>
2025-09-10 20:09:08 +0330

    14 longest common prefix

--- js/14-longestCommonPrefix.js
+++ js/14-longestCommonPrefix.js
@@ -3,9 +3,20 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
-    
-};
+  if (!strs || !strs.length) return "";
 
+  let prefix = strs[0];
+
+  for (let i = 0; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      console.log({ prefix, target: strs[i] });
+      prefix = prefix.slice(0, -1);
+      if (prefix === "") return "";
+    }
+  }
+
+  return prefix;
+};
 
 // Example 1:
 
@@ -16,4 +27,5 @@ var longestCommonPrefix = function (strs) {
 // Input: strs = ["dog","racecar","car"]
 // Output: ""
 // Explanation: There is no common prefix among the input strings.
- 
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
